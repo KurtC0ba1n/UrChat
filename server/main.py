24c60db8f68b0f9990e9992b4f1d70ab3086db4f
Victor804 <victor.vaucel@gmail.com>
2019-09-03 23:17:47 +0200

    add client for server

--- server/main.py
+++ server/main.py
@@ -1,7 +1,19 @@
+import socket
+
 class Server:
-    def __init__(self):
-        self.host = ""
-        self.port = 12800
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+        self.clients = {}
 
         self.main_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.main_connection.bind((self.host, self.port))
+
+
+    def add_client(self, id, password):
+        if id not in self.clients:
+            self.clients[id] = {"password":password, "statut":0, "ip":""}
+            return True
+        else:
+            return False        
